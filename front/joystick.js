37b30fac0600744e44ec95b161d7071f9170c769
Motzee <perronm26@protonmail.ch>
2017-08-18 16:19:54 +0200

    Correctif fonction : deux events, un pour l'appui, un pour le relâchement de bouton

--- front/joystick.js
+++ front/joystick.js
@@ -1,11 +1,10 @@
-console.log('Hey !') ;
-
 let btns = document.getElementsByTagName("button");
 
-console.log(btns) ;
-
 for (let i = 0 ; i < btns.length ; i += 1)   {
-    btns[i].addEventListener("click", function(){
-        console.log(this.id) ;
+    btns[i].addEventListener("mousedown", function(){
+        console.log(this.id+"go") ;
+    });
+    btns[i].addEventListener("mouseup", function(){
+        console.log(this.id+"stop") ;
     });
 }
